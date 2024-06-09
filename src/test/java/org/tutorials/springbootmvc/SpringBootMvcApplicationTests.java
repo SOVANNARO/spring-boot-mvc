69e61c0120c73ce5b90ab46b2e8c4d7478e3d0e6
SOVANNARO <sovannaro066@gmail.com>
2024-06-09 16:15:40 +0700

    init spring mvc

---
+++ src/test/java/org/tutorials/springbootmvc/SpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tutorials.springbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
